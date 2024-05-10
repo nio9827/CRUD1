2b783961561adf431d0a664cf10ecb42b3e7b505
anibal <anibal.alveo1998@gmail.com>
2024-05-10 16:01:43 -0500

    commit "10/04/2024"

--- CRUD1app/urls.py
+++ CRUD1app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('plantilla', views.plantilla, name='plantilla'),
     path('home', views.home, name='home'),
     path('listar', views.listar, name='listar'),
-    path('crear', views.crear, name='crear'),
+    path('c_modelo', views.c_modelo, name='c_modelo'),
+    path('c_usuario', views.c_usuario, name='c_usuario'),
+    path('c_equipo', views.c_equipo, name='c_equipo'),
 ]
--- CRUD1app/views.py
+++ CRUD1app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import *
 
 # Create your views here.
 
@@ -14,5 +16,29 @@ def listar(request):
     return render(request, 'lista.html')
 
 
-def crear (request):
-    return render (request,'crear.html')
+
+def c_modelo (request):
+    # if this is a POST request we need to process the form data
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect("/thanks/")
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render (request, "crear/c_modelo.html", {"form": form})
+
+
+def c_usuario(request):
+    return render (request,'crear/c_usuario.html')
+
+
+def c_equipo(request):
+    return render (request,'crear/c_equipo.html')
